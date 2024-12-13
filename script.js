e5f247dce5ff895b8152cee16128192566e740e2
Ravisharma-1999 <ravi829144@gmail.com>
2024-12-13 11:54:23 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
 //your JS code here. If required.
+function removeColor(){
+const colorSelect = document.getElementById("colorSelect");
+const selectedIndex = colorSelect.selectedIndex;
+if(selectedIndex !== -1){
+	btn.remove(selectIndex)
+}
+}
